2b88029b171cf001d78cba6b720057e913115b59
Emmet Susslin <esusslin@gmail.com>
2016-06-19 21:13:55 -0700

    pre-AlamoFire

--- ToolBelt_4.0/ToolBelt_4.0/ViewController.swift
+++ ToolBelt_4.0/ToolBelt_4.0/ViewController.swift
@@ -18,10 +18,10 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         return button
     }()
     
-    //    let email = ""
-    //    let first_name = ""
-    //    let last_name = ""
-    //    let image = ""
+        let email = ""
+        let first_name = ""
+        let last_name = ""
+        let image = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,10 +42,10 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
         FBSDKGraphRequest(graphPath: "me", parameters: parameters).startWithCompletionHandler { (connection, result, error) -> Void in
             
-            //            let email = (result["email"] as? String)!
-            //            let first_name = (result["first_name"] as? String)!
-            //            let last_name = (result["last_name"] as? String)!
-            //            let image = (result["picture"]!!["data"]!!["url"] as? String)!
+                        let email = (result["email"] as? String)!
+                        let first_name = (result["first_name"] as? String)!
+                        let last_name = (result["last_name"] as? String)!
+                        let image = (result["picture"]!!["data"]!!["url"] as? String)!
             
             if error != nil {
                 print(error)
@@ -54,9 +54,12 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
             
             if let email = result["email"] as? String {
                 print(email)
+                print(first_name)
+                print(last_name)
+                print(image)
             }
             
-            print(result)
+//            print(result)
         }
     }
     
